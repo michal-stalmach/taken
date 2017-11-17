1a722b9b79c24a77f2a38c7760bf18cff16ab1c9
Filip Mamcarczyk <filip.mamcarczyk@codewise.com>
2017-11-17 11:14:48 +0100

    Working sidebar

--- src/app/main/services/sidebar.service.ts
+++ src/app/main/services/sidebar.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Subject, BehaviorSubject } from "rxjs";
+import {BehaviorSubject } from "rxjs";
 
 @Injectable()
 export class SidebarService {
 
   public isOpen: boolean = false;
-  public isOpen$: BehaviorSubject<boolean> = new Subject<boolean>(false);
+  public isOpen$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
 
   constructor() { }
 
@@ -13,4 +13,15 @@ export class SidebarService {
     this.isOpen = !this.isOpen;
     this.isOpen$.next(this.isOpen);
   }
+
+  public open(): void {
+    this.isOpen = true;
+    this.isOpen$.next(this.isOpen);
+  }
+
+  public close(): void {
+    this.isOpen = false;
+    this.isOpen$.next(this.isOpen);
+  }
+
 }
